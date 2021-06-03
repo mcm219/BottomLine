6280e9313436a9818199ecbae3ade99e3c76c004
Matt McIntire <matt@mattmcintire.com>
2021-06-03 00:58:13 -0400

    BL-72: created initial vehicle make data model

--- bottomline/blweb/models.py
+++ bottomline/blweb/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class VehicleMake(models.Model):
+    """A brand of vehicle manufacture"""
+    name = models.CharField(max_length=50,
+                            help_text="The name of the vehicle make")
+    website = models.URLField(help_text="The official website for this vehicle make")
+
