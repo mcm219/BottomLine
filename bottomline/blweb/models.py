2b219be74fc144538db34fda5a16d00f0034515a
Matt McIntire <matt@mattmcintire.com>
2021-06-03 23:51:15 -0400

    BL-72: added VehicleModel to the data model

--- bottomline/blweb/models.py
+++ bottomline/blweb/models.py
@@ -6,3 +6,11 @@ class VehicleMake(models.Model):
                             help_text="The name of the vehicle make")
     website = models.URLField(help_text="The official website for this vehicle make")
 
+class VehicleModel(models.Model):
+    """A model of vehicle for a particular make"""
+    name = models.CharField(max_length=100,
+                            help_text="The name of the vehicle model")
+    year = models.IntegerField(help_text="The model year for this model")
+
+
+
