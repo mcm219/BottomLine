7760dfc04b50ede32e4f809a7e889a110d100e8e
Matt McIntire <matt@mattmcintire.com>
2021-06-05 22:09:45 -0400

    BL-74: registered the data models for the vehicle information (make, model, options) with the admin site to allow access to CRUD operations for these fields by a site admin per the subtask directive.

--- bottomline/blweb/admin.py
+++ bottomline/blweb/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from blweb.models import VehicleModel, VehicleOption, VehicleMake
+
+admin.site.register(VehicleModel)
+admin.site.register(VehicleMake)
+admin.site.register(VehicleOption)
+
