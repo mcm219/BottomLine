de9dc6af9c45863dc68f25b0161dd223ed743b9a
Matt McIntire <matt@mattmcintire.com>
2021-06-13 10:41:16 -0400

    BL-28: reusing and modified the Django admin templates. Updated the INSTALLED_APPS config to place blweb at top of search path.

--- bottomline/bottomline/settings.py
+++ bottomline/bottomline/settings.py
@@ -30,14 +30,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blweb',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blweb',
-]
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
