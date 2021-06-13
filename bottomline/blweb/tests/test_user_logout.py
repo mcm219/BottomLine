25cafd9dea3aa25437b4f6779777f93b3849f862
Matt McIntire <matt@mattmcintire.com>
2021-06-13 14:21:22 -0400

    BL-67: created module and stubbed out test case methods.

---
+++ bottomline/blweb/tests/test_user_logout.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+
+
+# Test class for unit tests on the user login interface
+class TestUserLogout(TestCase):
+
+    # any needed setup for the tests. this function will be run before every test case
+    def SetUp(self):
+        pass
+
+    # check to see if the page is present
+    def test_logout_page_present(self):
+        pass
+
+    # check that a user can log out
+    def test_user_log_out_success(self):
+        pass
+
+    # check that once logged out, the profile page is blank
+    def test_user_log_out_view_profile(self):
+        pass
+
+
