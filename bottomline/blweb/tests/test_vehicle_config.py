47e555b256a4fd23020a749b51f672f882d1aaff
Matt McIntire <matt@mattmcintire.com>
2021-06-26 12:53:33 -0400

    BL-106: created testing stubs for vehicle model unit tests.

---
+++ bottomline/blweb/tests/test_vehicle_config.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from blweb.models import VehicleMake
+from blweb.models import VehicleModel
+from blweb.models import VehicleOption
+
+
+# Test class for unit tests on the vehicle data model classes
+class TestVehicleConfig(TestCase):
+
+    # CRUD tests
+    # set up the environment
+    def setUp(self):
+        pass
+
+
+
