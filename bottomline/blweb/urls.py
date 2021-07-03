64dbb1560a1b26205446afe9b26cf1e71466c578
Matt McIntire <matt@mattmcintire.com>
2021-07-03 00:08:58 -0400

    BL-108: Added routes to urls.py for the new templates.

--- bottomline/blweb/urls.py
+++ bottomline/blweb/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [path('', views.index, name='landing'),
                path('accounts/account_signup/', views.account_signup, name='account_signup'),
                path('accounts/dealer_signup/', views.dealer_signup, name='dealer_signup'),
                path('vehicle_config/', views.vehicle_config, name='vehicle_config'),
+               path('vehicle_config_model/', views.vehicle_config_model, name='vehicle_config_model'),
+               path('vehicle_config_options/', views.vehicle_config_options, name='vehicle_config_options'),
                ]
 
