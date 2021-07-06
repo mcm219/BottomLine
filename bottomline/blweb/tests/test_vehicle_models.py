8e89eb61e1ccd69566b1fe7ea51baf6d17b69571
Matt McIntire <matt@mattmcintire.com>
2021-07-05 23:52:32 -0400

    BL-99: added unit tests to test_vehicle_models.py for the VehicleColor class derived from VehicleOption.

--- bottomline/blweb/tests/test_vehicle_models.py
+++ bottomline/blweb/tests/test_vehicle_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from blweb.models import VehicleMake
+from blweb.models import VehicleMake, VehicleColor
 from blweb.models import VehicleModel
 from blweb.models import VehicleOption
 
@@ -100,3 +100,45 @@ class TestVehicleOptions(TestCase):
     # cleanup the test environment
     def tearDown(self):
         pass
+
+
+class TestVehicleColors(TestCase):
+    # set up the test environment
+    def setUp(self):
+        self.ford = VehicleMake.objects.create(name="Ford", website="www.ford.com")
+        self.mustang = VehicleModel.objects.create(name="Mustang", year=2021, make=self.ford)
+        self.pp1 = VehicleOption.objects.create(name="Performance Pack 1",
+                                                description="Brakes, shocks, and spoiler added",
+                                                price=2000,
+                                                model=self.mustang)
+        self.kona_blue = VehicleColor.objects.create(name="Kona Blue",
+                                                     description="Kona Blue",
+                                                     price="250",
+                                                     model=self.mustang)
+
+    # test that we can create a new vehicle color
+    def test_create_vehicle_color(self):
+        self.assertIsInstance(self.kona_blue, VehicleOption)
+
+    # test that we can read a vehicle color from the model
+    def test_select_vehicle_color(self):
+        self.assertEqual(str(self.kona_blue), "Kona Blue")
+
+    # test that the vehicle model is linked to the color
+    def test_vehicle_model_from_color(self):
+        self.assertEqual(self.kona_blue.model, self.mustang)
+
+    # test that we can delete a vehicle color
+    def test_delete_vehicle_color(self):
+        # first delete the object
+        self.kona_blue.delete()
+
+        # now make sure we can't get any more objects
+        self.assertEqual(VehicleColor.objects.count(), 0)
+
+    # test that a vehicle color is-a vehicle option, but a vehicle option is not a color
+    def test_inheritance_vehicle_color(self):
+        self.assertTrue(issubclass(type(self.kona_blue), VehicleOption))
+        self.assertFalse(type(self.pp1) == VehicleColor)
+
+
