97547d2f6e192e4732cfbf183a3b13376e1d7c0c
Matt McIntire <matt@mattmcintire.com>
2021-07-10 00:38:05 -0400

    BL-98: added some unit tests.

--- bottomline/blweb/tests/test_vehicle_config_options.py
+++ bottomline/blweb/tests/test_vehicle_config_options.py
@@ -13,13 +13,33 @@ class TestVehicleConfigOptions(TestCase):
     # set up the environment
     def setUp(self):
         self.client = Client()
-        self.veh_make = "BMW"
-        self.veh_model = "M3"
+        # first create a test make
+        self.make = VehicleMake.objects.create(
+            name='Ferrari',
+            website='www.ferrari.com',
+        )
+        # now a test model
+        self.model = VehicleModel.objects.create(
+            name='Roma',
+            year=2021,
+            make=self.make
+        )
 
     # test that the page is found
     def test_options_page_exists(self):
-        pass
+        # create a test VehicleConfig from the make above
+        veh_config = VehicleConfig.objects.create(make=self.make,
+                                                  model=self.model)
+        veh_config.save()
 
+        # set the required session variable
+        session = self.client.session
+        session['vehicle_config'] = veh_config.pk
+        session.save()
+
+        response = self.client.get('/vehicle_config_options/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template_name='vehicle_config_options.html')
 
     def test_something_else(self):
         pass
