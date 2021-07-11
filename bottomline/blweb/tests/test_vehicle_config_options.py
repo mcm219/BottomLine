c772c26a27c2f3b4cea43a11e9f01ecfd37d2add
Matt McIntire <matt@mattmcintire.com>
2021-07-11 15:55:12 -0400

    BL-98, BL-99: Added more unit tests.

--- bottomline/blweb/tests/test_vehicle_config_options.py
+++ bottomline/blweb/tests/test_vehicle_config_options.py
@@ -2,8 +2,8 @@ from django.http import SimpleCookie
 from django.test import TestCase
 from django.test import Client
 
-from blweb.forms import VehicleMakeForm, VehicleModelForm
-from blweb.models import VehicleMake, VehicleConfig
+from blweb.forms import VehicleOptionsForm, VehicleColorOptionsForm
+from blweb.models import VehicleMake, VehicleConfig, VehicleColor
 from blweb.models import VehicleModel
 from blweb.models import VehicleOption
 
@@ -25,6 +25,31 @@ class TestVehicleConfigOptions(TestCase):
             make=self.make
         )
 
+        self.option1 = VehicleOption.objects.create(name='Carbon Buckets',
+                                                    description='Race-inspired carbon fiber bucket seats',
+                                                    price=5000,
+                                                    model=self.model)
+
+        self.option2 = VehicleOption.objects.create(name='Shadowline Trim Package',
+                                                    description='Enhance the look of your Ferrari with blacked out trim',
+                                                    price=4350,
+                                                    model=self.model)
+
+        self.paint_color = VehicleColor.objects.create(name='Rosso Corsa',
+                                                       description='The original color of Ferrari Racing',
+                                                       price=1000,
+                                                       model=self.model)
+
+        self.ford = VehicleMake.objects.create(name='Ford',
+                                               website='www.ford.com')
+        self.mustang = VehicleModel.objects.create(name='Mustang',
+                                                   year=2021,
+                                                   make=self.ford)
+        self.pp1 = VehicleOption.objects.create(name='Performance Pack 1',
+                                                description='Mustang Performance Pack 1',
+                                                price=3000,
+                                                model=self.mustang)
+
     # test that the page is found
     def test_options_page_exists(self):
         # create a test VehicleConfig from the make above
@@ -41,5 +66,50 @@ class TestVehicleConfigOptions(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, template_name='vehicle_config_options.html')
 
-    def test_something_else(self):
-        pass
+    def test_options_form(self):
+        # create a test VehicleConfig from the make above
+        veh_config = VehicleConfig.objects.create(make=self.make,
+                                                  model=self.model,
+                                                  color=self.paint_color)
+        veh_config.save()
+
+        # set the required session variable
+        session = self.client.session
+        session['vehicle_config'] = veh_config.pk
+        session.save()
+
+        form = VehicleOptionsForm(prefix='options',
+                                  chosen_model=veh_config.model.pk,
+                                  data={'options-options': [self.option1.pk, self.option2.pk]})
+
+        self.assertTrue(form.is_valid())
+
+    def test_colors_form(self):
+        # create a test VehicleConfig from the make above
+        veh_config = VehicleConfig.objects.create(make=self.make,
+                                                  model=self.model,
+                                                  color=self.paint_color)
+        veh_config.save()
+
+        # set the required session variable
+        session = self.client.session
+        session['vehicle_config'] = veh_config.pk
+        session.save()
+
+        form = VehicleColorOptionsForm(prefix='colors',
+                                       chosen_model=veh_config.model.pk,
+                                       data={'colors': self.paint_color.pk})
+
+        self.assertTrue(form.is_valid())
+
+    # test the options form field names
+    def test_vehicle_options_form_fields(self):
+        form = VehicleOptionsForm()
+        self.assertIn("options", form.fields)
+
+    # test the colors form field names
+    def test_vehicle_color_options_form_fields(self):
+        form = VehicleColorOptionsForm()
+        self.assertIn("colors", form.fields)
+
+
