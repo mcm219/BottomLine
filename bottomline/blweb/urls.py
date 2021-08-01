e41fb0a394498a6ce4114728bd7c63902eeb1341
Matt McIntire <matt@mattmcintire.com>
2021-08-01 14:45:42 -0400

    BL-88, BL-92, BL-97: Added new template for editing user profiles. Updated original profile page to show added data fields (address and dealer affiliation).

--- bottomline/blweb/urls.py
+++ bottomline/blweb/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [path('', views.index, name='landing'),
                path('accounts/profile/', views.profile, name='profile'),
+               path('accounts/profile/edit/', views.profile_edit, name='profile_edit'),
                path('accounts/account_signup/', views.account_signup, name='account_signup'),
                path('accounts/dealer_signup/', views.dealer_signup, name='dealer_signup'),
                path('vehicle_config/', views.vehicle_config, name='vehicle_config'),
