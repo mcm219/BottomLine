c4b3e3ccb0d9532d2974d17cd609503a642d9fe1
Matt McIntire <matt@mattmcintire.com>
2021-08-07 11:29:05 -0400

    BL-112: Added unit tests to verify behavior of vehicle config history page.

--- bottomline/blweb/tests/test_vehicle_config_history.py
+++ bottomline/blweb/tests/test_vehicle_config_history.py
@@ -1,10 +1,12 @@
+from django.contrib.auth.models import User
 from django.http import SimpleCookie
 from django.template.loader import render_to_string
 from django.test import TestCase
 from django.test import Client
+from django.urls import reverse
 
 from blweb.forms import VehicleOptionsForm, VehicleColorOptionsForm
-from blweb.models import VehicleMake, VehicleConfig, VehicleColor
+from blweb.models import VehicleMake, VehicleConfig, VehicleColor, AccountType
 from blweb.models import VehicleModel
 from blweb.models import VehicleOption
 
@@ -28,6 +30,7 @@ class TestVehicleConfigHistory(TestCase):
             make=self.make
         )
 
+        # create some test options
         self.option1 = VehicleOption.objects.create(name='Carbon Buckets',
                                                     description='Race-inspired carbon fiber bucket seats',
                                                     price=5000,
@@ -52,3 +55,97 @@ class TestVehicleConfigHistory(TestCase):
                                                 description='Mustang Performance Pack 1',
                                                 price=3000,
                                                 model=self.mustang)
+
+        # create a user
+        self.test_shopper = User.objects.create_user(username='testshopper', password='test@#628password')
+        self.test_shopper.profile.account_type = AccountType.SHOPPER.value
+        self.test_shopper.save()
+
+    # check to see if the page is present
+    def test_config_history_page_present(self):
+        # user needs to be logged in to see the page
+        self.client.force_login(self.test_shopper)
+
+        response = self.client.get('/view_configs/')
+        self.assertEqual(response.status_code, 200)
+
+    # check that the proper view was used (dealer)
+    def test_config_history_view_name(self):
+        # user needs to be logged in to see the page
+        self.client.force_login(self.test_shopper)
+
+        response = self.client.get(reverse('view_configs'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template_name='view_configs.html')
+
+    # check to see that the Profile link is present on the profile page
+    def test_config_history_link(self):
+        # user needs to be logged in to see the page
+        self.client.force_login(self.test_shopper)
+
+        response = self.client.get(reverse('profile'))
+        self.assertContains(response, text="View my vehicle configurations")
+
+    # check that the config history page does not load when logged out
+    def test_config_history_logged_out(self):
+        # user needs to be logged in to see the page
+        self.client.logout()
+
+        response = self.client.get(reverse('view_configs'))
+        self.assertEqual(response.status_code, 302)
+
+    # check to see if the page has the test config
+    def test_config_history_config_present(self):
+        # user needs to be logged in to see the page
+        self.client.force_login(self.test_shopper)
+
+        # create a test VehicleConfig from the make above
+        veh_config = VehicleConfig.objects.create(make=self.make,
+                                                  model=self.model,
+                                                  color=self.paint_color,
+                                                  user=self.test_shopper,
+                                                  config_name='My Test Config!')
+        veh_config.save()
+
+        response = self.client.get('/view_configs/')
+        self.assertEqual(response.status_code, 200)
+
+        self.assertContains(response, text='My Test Config')
+
+    # check that the GET parameter passing works
+    def test_config_history_get_params(self):
+        # user needs to be logged in to see the page
+        self.client.force_login(self.test_shopper)
+
+        # create a test VehicleConfig from the make above
+        veh_config = VehicleConfig.objects.create(make=self.make,
+                                                  model=self.model,
+                                                  color=self.paint_color,
+                                                  user=self.test_shopper,
+                                                  config_name='My Test Config!')
+        veh_config.save()
+
+        url = f'/vehicle_config_complete/?config_id={veh_config.pk}'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertContains(response, text='Ferrari')
+
+    # check that the config history page contains the proper link to the details for the config shown
+    def test_config_history_link_to_details(self):
+        # user needs to be logged in to see the page
+        self.client.force_login(self.test_shopper)
+
+        # create a test VehicleConfig from the make above
+        veh_config = VehicleConfig.objects.create(make=self.make,
+                                                  model=self.model,
+                                                  color=self.paint_color,
+                                                  user=self.test_shopper,
+                                                  config_name='My Test Config!')
+        veh_config.save()
+
+        url = f'/vehicle_config_complete/?config_id={veh_config.pk}'
+        response = self.client.get('/view_configs/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, text=url)
+
